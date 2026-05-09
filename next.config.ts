c96cf858674f32c5a44f445bbab01c45d264b3ae
Abhijit0405 <vaibhavkale625@gmail.com>
2026-05-10 00:58:27 +0530

    fix: remove invalid skipWaiting property from next-pwa config

--- next.config.ts
+++ next.config.ts
@@ -44,7 +44,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const withPWA = withPWAInit({
   dest: 'public',
   register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
   workboxOptions: {
     disableDevLogs: true,
